3d144a9fb151c3501a4c4490835d5c882850507d
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2020-07-20 15:40:58 -0300

    ok

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,9 +4,13 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
 	
@@ -27,8 +31,21 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
+		
+		// Cenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+		
+		// Ação
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		// Verificação
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 		
 	}
 }
